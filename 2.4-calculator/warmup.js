7570e73e430743d8430f9e9be0a1681def7563a5
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-08 14:15:47 -0600

    My initial commit for my index.html

---
+++ 2.4-calculator/warmup.js
@@ -0,0 +1,9 @@
+// Using this code as a starting point...
+//   Console log out "Jason Vanderslice" as a single string
+//   You have until 9:15am
+  var fullNameArray = ["Jason", "Vanderslice"];
+
+for (var i = 0; i < fullNameArray.length; i++) {
+var seperateName = fullNameArray[i]
+  }
+console.log(seperateName);
