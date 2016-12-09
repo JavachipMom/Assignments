4d8b82b8a3710559acbc050afc6f418ccd00325b
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-08 22:41:49 -0600

    Sorry again, I promise I'm going to get better at committing. Worked on adding values and connecting with buttons.

--- 2.4-calculator/main.js
+++ 2.4-calculator/main.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var nameButtonElements = document.querySelectorAll("[]")
+  var nameButtonElement = document.querySelector("[data-js='button__7']");
+  var outputNumberElement = document.querySelector("[data-js='display__value']");
+  nameButtonElement.addEventListener("click", function(){
+    outputNumberElement.textContent += nameButtonElement.textContent;
+    nameButtonElement = document.querySelector("[data-js='button__8']");
+    nameButtonElement.addEventListener("click", function(){
+    outputNumberElement.textContent += nameButtonElement.textContent;
+    nameButtonElement = document.querySelector("[data-js='button__9']");
+    nameButtonElement.addEventListener("click", function(){
+    outputNumberElement.textContent += nameButtonElement.textContent;
+    nameButtonElement = document.querySelector("[data-js='button__4']");
+    nameButtonElement.addEventListener("click", function(){
+    outputNumberElement.textContent += nameButtonElement.textContent;
+    });
+      });
+    });
+  });
 })
