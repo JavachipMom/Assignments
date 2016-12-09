0f41749706bdddfb7678e4c0f9623c16adb2eb1b
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-09 13:09:20 -0600

    My fixed placeholder values instead of strings. I am REALLY REALLY REALLY sorry about not having any psuedocode, I will get better at tracking a todo list.

--- 2.4-calculator/main.js
+++ 2.4-calculator/main.js
@@ -1,82 +1,88 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var nameButton7Element = document.querySelector("[data-js='button__7']");
-  var outputNumberElement = document.querySelector("[data-js='display__value']");
-  nameButton7Element.addEventListener("click", function(){
-    outputNumberElement.textContent += nameButton7Element.textContent;
-    });
   var nameButtonClearElement = document.querySelector("[data-js='button__clear']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButtonClearElement.addEventListener("click", function(){
       outputNumberElement.textContent = "";
     })
+  var nameButton7Element = document.querySelector("[data-js='button__7']");
+  var outputNumberElement = document.querySelector("[data-js='display__value']");
+  nameButton7Element.addEventListener("click", function(){
+    outputNumberElement.textContent += 7;
+    });
   var nameButton8Element = document.querySelector("[data-js='button__8']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton8Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton8Element.textContent;
+      outputNumberElement.textContent += 8;
     });
   var nameButton9Element = document.querySelector("[data-js='button__9']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton9Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton9Element.textContent;
+      outputNumberElement.textContent += 9;
     });
   var nameButtonDivElement = document.querySelector("[data-js='button__/']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButtonDivElement.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButtonDivElement.textContent;
+      outputNumberElement.textContent += "/";
     });
   var nameButton4Element = document.querySelector("[data-js='button__4']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton4Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton4Element.textContent;
+      outputNumberElement.textContent += 4;
     });
   var nameButton5Element = document.querySelector("[data-js='button__5']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton5Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton5Element.textContent;
+      outputNumberElement.textContent += 5;
     });
   var nameButton6Element = document.querySelector("[data-js='button__6']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton6Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton6Element.textContent;
+      outputNumberElement.textContent += 6;
     });
   var nameButtonMultiElement = document.querySelector("[data-js='button__*']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButtonMultiElement.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButtonMultiElement.textContent;
+      outputNumberElement.textContent += "*";
     });
   var nameButton1Element = document.querySelector("[data-js='button__1']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton1Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton1Element.textContent;
+      outputNumberElement.textContent += 1;
     });
   var nameButton2Element = document.querySelector("[data-js='button__2']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton2Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton2Element.textContent;
+      outputNumberElement.textContent += 2;
     });
   var nameButton3Element = document.querySelector("[data-js='button__3']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton3Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton3Element.textContent;
+      outputNumberElement.textContent += 3;
     });
+  var nameButtonMinusElement = document.querySelector("[data-js='button__-']");
+  var outputNumberElement = document.querySelector("[data-js='display__value']");
+    nameButtonMinusElement.addEventListener("click", function(){
+    outputNumberElement.textContent += nameButtonMinusElement.textContent;
+      })
   var nameButton0Element = document.querySelector("[data-js='button__0']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButton0Element.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButton0Element.textContent;
+      outputNumberElement.textContent += 0;
     });
-  var nameButtonDecimalElement = document.querySelector("[data-js='.']");
+  var nameButtonDecimalElement = document.querySelector("[data-js='button__.']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
-    nameButtonDecimalElement.addEventListener("click", function(){
-      outputNumberElement.textContent += nameButtonDecimalElement.textContent;
-    });
-  var nameButtonEqualElement = document.querySelector("[data-js='=']");
-  var outputNumberElement = document.querySelector("[data-js='display__value']");
-    nameButtonEqualElement.addEventListener("click", function(){
-      outputNumberElement.textContent == nameButtonEqualElement.textContent;
+  nameButtonDecimalElement.addEventListener("click", function(){
+      outputNumberElement.textContent += ".";
     });
   var nameButtonPlusElement = document.querySelector("[data-js='button__+']");
   var outputNumberElement = document.querySelector("[data-js='display__value']");
     nameButtonPlusElement.addEventListener("click", function(){
       outputNumberElement.textContent += nameButtonPlusElement.textContent;
     });
+  var nameButtonEqualElement = document.querySelector("[data-js='button__=']");
+  var outputNumberElement = document.querySelector("[data-js='display__value']");
+    nameButtonEqualElement.addEventListener("click", function(){
+      var Equals = eval(outputNumberElement.textContent);
+        outputNumberElement.textContent = Equals
+      });
   });
