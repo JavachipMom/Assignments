0b85c2fe1fc8bfeedf79296360b08ee86cf8cc68
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-13 16:56:53 -0600

    Established my for loop and was able to obtain my data from JSON

--- 3.2-form-builder/main.js
+++ 3.2-form-builder/main.js
@@ -1,16 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  var formData = document.querySelector("[data-js='form']");
+  var formElement = document.querySelector("[data-js='form']");
 
   var xhr = new XMLHttpRequest();
 
-   xhr.open("GET", "http://json-data.herokuapp.com/forms");
+  xhr.open("GET", "http://json-data.herokuapp.com/forms");
   //  console.log(xhr);
-   xhr.addEventListener("load", function(e){
+  xhr.addEventListener("load", function(e){
+  // create variable to this.response to look up and take info
+    var xhrData = this.response;
+  // Made a parsed variable referenced to JSON
+    var JSONData = JSON.parse(xhrData);
+  // Established my palceholder
+    var formPlaceholder = "";
+  // Created my for loop statement
+    for (var i = 0; i < JSONData.length; i++) {
+      var inputData = JSONData[i];
+      var inputTypeHTML = `<label>${inputData.label}</label><input type="${inputData.type}" id="${inputData.id}" icon="${inputData.icon}">
+      `;
+    // for (var i = 0; i < inputTypeHTML.length; i++) {
+    //   var selectData = inputTypeHTML[4];
+    //
+    // }
+      // concatenate inputHTML to placeholder HTML
+      formPlaceholder += inputTypeHTML;
 
-     var xhrData = this.response;
-     var JSONData = JSON.parse(xhrData);
-     console.log(JSONData);
-   })
+    }
+    // So put my formPlaceholder into my formElement
+    formElement.innerHTML += formPlaceholder;
+  })
   xhr.send();
 })
