0f4e0cc9d67aa06c5148abf2c24b73cccccd04fc
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-16 15:20:00 -0600

    Forgot to commit this entire time, adjusting my JavaScript

---
+++ 3.4-jquery-holiday-buying-list/main.js
@@ -0,0 +1,31 @@
+// First I need to make my DOM ready with jQuery in my JavaScript
+$(function(){
+  // Create my variable to pull my HTML body over to my JavaScript
+  var $body = $("body");
+  // Created my variables to look for elements in my body of the JavaScript
+  var $listForm = $body.find("[data-js='listForm']");
+  var $userList = $body.find("[data-js='userList']");
+  var $addButton = $body.find("[data-js='addButton']");
+  var $itemsElement = $body.find("[data-js='items']");
+// Add event listener for click and prevent the default of refreshing
+$addButton.on("click", function(e){
+  e.preventDefault();
+// Create a new article when the button is clicked with the value inside of it
+var 
+// Create variables for elements of the event I want to happen when the button is clicked by posting my users items on the list.
+  var $inputCheckboxElement = $("<input>")
+    .attr({
+      class: "circle",
+      "data-js": "circle",
+      type: "checkbox",
+      value: ""
+    });
+
+  var $inputItemArticle = $("<article>")
+    .attr({
+      class: "giftItem",
+      "data-js": "giftItem",
+    });
+
+})
+})
