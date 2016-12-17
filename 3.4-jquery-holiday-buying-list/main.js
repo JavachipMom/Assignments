cd6b5b062ea38e7672cad9b7e724da588fd5a36e
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-17 15:54:50 -0600

    Split my arrays to seperate my gift item from price

--- 3.4-jquery-holiday-buying-list/main.js
+++ 3.4-jquery-holiday-buying-list/main.js
@@ -4,15 +4,22 @@ $(function(){
   var $body = $("body");
   // Created my variables to look for elements in my body of the JavaScript
   var $listForm = $body.find("[data-js='listForm']");
-  var $userList = $body.find("[data-js='userList']");
+  var $userGiftInput = $body.find("[data-js='userGiftInput']");
   var $addButton = $body.find("[data-js='addButton']");
   var $itemsElement = $body.find("[data-js='items']");
 // Add event listener for click and prevent the default of refreshing
 $addButton.on("click", function(e){
   e.preventDefault();
 // Create a variable for the new article when the button is clicked with the value inside of it
-var itemsEntered = $userList.val();
+var itemsEntered = $userGiftInput.val();
 // Create variables for elements of the event I want to happen when the button is clicked by posting my users items on the list.
+// So now I need to split my $itemName and $itemValue apart.
+var giftItemArray = itemsEntered.split(",");
+// Now I want to make a variable for each index in the array
+var giftItemName = giftItemArray[0];
+// Now create a variable for the second index in my array
+var giftItemPrice = giftItemArray[1];
+// Now I need to actually separate my indexes
   var $inputCheckboxElement = $("<input>")
     .attr({
       class: "circle",
@@ -29,13 +36,13 @@ var itemsEntered = $userList.val();
     .attr({
       class: "itemName",
       "data-js": "itemsWanted"
-    }).text(itemsEntered)
+    }).text(giftItemName)
     ;
   var $itemValue = $("<p>")
     .attr({
       class: "value",
       "data-js": "price"
-    }).text(itemsEntered)
+    }).text(giftItemPrice)
     ;
     // Need to take all variables and add to list
   $inputItemArticle
@@ -45,5 +52,5 @@ var itemsEntered = $userList.val();
     ;
 
     $itemsElement.prepend($inputItemArticle)
-});
+  });
 })
