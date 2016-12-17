334079470983032c47cc2b00820791ee8c392510
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-17 14:53:10 -0600

    FINALLY got this CRAP to workgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -pgit add -p

--- 3.4-jquery-holiday-buying-list/main.js
+++ 3.4-jquery-holiday-buying-list/main.js
@@ -10,8 +10,8 @@ $(function(){
 // Add event listener for click and prevent the default of refreshing
 $addButton.on("click", function(e){
   e.preventDefault();
-// Create a new article when the button is clicked with the value inside of it
-var 
+// Create a variable for the new article when the button is clicked with the value inside of it
+var itemsEntered = $userList.val();
 // Create variables for elements of the event I want to happen when the button is clicked by posting my users items on the list.
   var $inputCheckboxElement = $("<input>")
     .attr({
@@ -20,12 +20,30 @@ var
       type: "checkbox",
       value: ""
     });
-
   var $inputItemArticle = $("<article>")
     .attr({
       class: "giftItem",
       "data-js": "giftItem",
     });
+  var $itemName = $("<p>")
+    .attr({
+      class: "itemName",
+      "data-js": "itemsWanted"
+    }).text(itemsEntered)
+    ;
+  var $itemValue = $("<p>")
+    .attr({
+      class: "value",
+      "data-js": "price"
+    }).text(itemsEntered)
+    ;
+    // Need to take all variables and add to list
+  $inputItemArticle
+    .append($inputCheckboxElement)
+    .append($itemName)
+    .append($itemValue)
+    ;
 
-})
+    $itemsElement.prepend($inputItemArticle)
+});
 })
