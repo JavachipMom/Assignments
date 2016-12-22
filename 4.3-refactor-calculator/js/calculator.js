7c6bb621af8019564d31ddbf5604762b73f77c64
JavachipMom <Nicolesp4ever@gmail.com>
2016-12-21 21:47:18 -0600

    pseudocode what I THINK I need to do next and commented out some sections in my main.js

--- 4.3-refactor-calculator/js/calculator.js
+++ 4.3-refactor-calculator/js/calculator.js
@@ -1,6 +1,13 @@
 var $ = require("jquery");
 $(function(){
-
+  // Create my variable to pull my html body over to my js with jQuery
   var $body = $("main__body");
-  console.log($body);
+  // Create variable to find form for calculator
+  var $form = $body.find("[data-js='calculator']");
+  var $clear = $body.find("[data-js='button__clear']");
+  // Create a variable for the output 'display__value'
+  var  $outputNumberElement = $body.find("[data-js='display__value']");
+  // Create a variable for my number element
+  var $numberElement = $body.find("[data-js='number']");
+  console.log($numberElement);
 })
