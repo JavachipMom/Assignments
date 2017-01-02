6b91a03cea7f4c177a606244aec5d2bfc75ef4c8
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-02 12:58:05 -0600

    Trying to fix my select options on my languages

--- 3.2-form-builder/main.js
+++ 3.2-form-builder/main.js
@@ -1,34 +1,45 @@
+// So I need to link my javascript with my html
 document.addEventListener("DOMContentLoaded", function(){
-
+  // Create a varibale to pull the data-js form over to javascript
   var formElement = document.querySelector("[data-js='form']");
-
+  // Create a variable
   var xhr = new XMLHttpRequest();
 
   xhr.open("GET", "http://json-data.herokuapp.com/forms");
   //  console.log(xhr);
   xhr.addEventListener("load", function(e){
-  // create variable to this.response to look up and take info
+    // create variable to this.response to look up and take info
     var xhrData = this.response;
-  // Made a parsed variable referenced to JSON
+    // Made a parsed variable referenced to JSON
     var JSONData = JSON.parse(xhrData);
-  // Established my palceholder
+    //Create other placeholder for all data
+    var allFormData = "";
+    // Established my form palceholder
     var formPlaceholder = "";
-  // Created my for loop statement
+    // Created my for loop statement
     for (var i = 0; i < JSONData.length; i++) {
       var inputData = JSONData[i];
       var inputTypeHTML = `<label>${inputData.label}</label><input type="${inputData.type}" id="${inputData.id}" icon="${inputData.icon}">
       `;
-  console.log(inputData);
-    // concatenate inputHTML to placeholder HTML
+      // concatenate inputHTML to placeholder HTML
       formPlaceholder += inputTypeHTML;
-    // Find a way to loop through the data again to pull selector from the data
-    // create a placeholder for it
+      // Create if statment to pull the selected data options
+      if (inputData.type === "select"){
+      // Find a way to loop through the data again to pull selector from the data
+        inputData.options.forEach(function(optionData){
+          // create a placeholder for it
+          formPlaceholder += `<option label="${optionData.label}" value="${optionData.value}"</options>;`
+        })
+      }else if(inputData.type === "text area"){
+        formPlaceholder += `<textarea class="textArea" placeholder="  ${inputData.label}" type="${inputData.type}"
+        id="${inputData.id}" icon="${inputData.icon}"></textarea>`;
+      }
     // reassign?? or concat with inputTypehtml?? Not too sure about this step
     // Since we have the selector, we need options
     // Create another loop to go through options of 'Language'
     // Create a placeholder again?? Then inject into
 
-
+    allFormData += formPlaceholder;
 
     }
     // So put my formPlaceholder into my formElement
