cb68b09bdf5ff8bd367f8f50a9ef2b557bc447e7
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-05 18:18:01 -0600

    Did most of my html work in my main.js with react

---
+++ 5.3-gift-react/js/main.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export default React.createClass({
+
+
+  render() {
+    return (
+      <main className="main">
+        <h1 className="listTitle"> GIFTS TO BUY
+        </h1>
+        <form action="index.html"
+              className="listForm"
+              method="post">
+          <input className="userGiftInput"
+                 type="text"
+                 name=""
+                 placeholder="Type the gift to buy HERE, and price HERE"
+                 ref="inputGift"/>
+          <input className="addButton"
+                 ref="inputAddItem"
+                 type="submit"
+                 value="ADD"
+                 onClick={ this.onAddButtonClick }/>
+        </form>
+        <section>
+          <article className="giftItem">
+            <input className="strikethrough"
+                   type="checkbox"
+                   value=""/>
+            <p className="itemName"> Blaze Monster Truck
+            </p>
+            <p className="value"> $49.99
+            </p>
+          </article>
+          <article className="giftItem">
+            <input className="circle"
+                   type="checkbox"
+                   value=""/>
+            <p className="itemName"> iPad mini 4
+            </p>
+            <p className="value"> $499
+            </p>
+          </article>
+          <article className="totalAmount">
+            <p className="total"> Total
+            </p>
+            <p className="priceAmount"> 0
+            </p>
+          </article>
+        </section>
+      </main>
+    )
+  }
+})
--- boilerplate-jasmine/webpack.config.js
+++ boilerplate-jasmine/webpack.config.js
@@ -6,3 +6,12 @@ module.exports = {
     filename: "bundle.js"
   }
 }
+  module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        }
+      ]
+    }
